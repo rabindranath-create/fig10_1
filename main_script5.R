e5354504bb3830479173f3316b18f95c1d580b7f
rabindranath-create <87770453+rabindranath-create@users.noreply.github.com>
2025-07-01 15:46:13 -0500

    Update main_script5.R

--- main_script5.R
+++ main_script5.R
@@ -38,7 +38,7 @@ results_1 <- data.frame(
 cost <- 5
 
 for (i in 1:100) {
-  set.seed(i)
+  
   obs_gen_para <- c(gamma = 0.3, d = 5, noPoints = 50, no_c = 25, no_o = 25)
   result <- ACS_Alg_M(obs_gen_para, kei = 1, lambda, cost)
   
@@ -70,7 +70,7 @@ results_2 <- data.frame(
 
 
 for (i in 1:100) {
-  set.seed(i)
+
   obs_gen_para <- c(gamma = 0.3, d = 5, noPoints = 100, no_c = 50, no_o = 50)
   result <- ACS_Alg_M(obs_gen_para, kei = 1, lambda, cost)
   
